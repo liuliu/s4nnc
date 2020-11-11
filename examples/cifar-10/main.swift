1c2d0adbcb73b489e0708534a5b330aced226c3e
Liu Liu <i@liuliu.me>
2020-11-11 15:43:49 -0500

    Make Array-based group work.

---
+++ examples/cifar-10/main.swift
@@ -0,0 +1,18 @@
+import nnc
+
+let graph = DynamicGraph()
+
+let tv1: DynamicGraph.Tensor<Float32> = graph.variable(.CPU, .C(1))
+tv1[0] = 10
+let tv1g0 = tv1.toGPU(0)
+let tv1g1 = tv1.toGPU(1)
+let tv2: DynamicGraph.Tensor<Float32> = graph.variable(.CPU, .C(1))
+tv2[0] = 2
+let tv2g0 = tv2.toGPU(0)
+let tv2g1 = tv2.toGPU(1)
+
+let rv = [tv1g0, tv1g1] .+ [tv2g0, tv2g1]
+
+let tv3c0 = rv[0].toCPU()
+let tv3c1 = rv[1].toCPU()
+print(rv)
--- nnc/Functional.swift
+++ nnc/Functional.swift
@@ -1,5 +1,10 @@
 import C_nnc
 
+/**
+ * This protocol defines a generic constructs such that we can use DynamicGraph.AnyTensorGroup
+ * to represent either a collection of tensors from multiple GPUs or one tensor.
+ * It has a typed version DynamicGraph.TensorGroup to enforce type constraint.
+ */
 public protocol DynamicGraph_AnyTensorGroup {
   associatedtype AnyTensor
   static func exec(cmd: ccv_nnc_cmd_t, hint: ccv_nnc_hint_t, inputs: [AnyTensor], outputSize: Int32, streamContext: StreamContext?) -> [AnyTensor]
@@ -56,9 +61,33 @@ extension DynamicGraph.AnyTensor: DynamicGraph.AnyTensorGroup {
 extension Array: DynamicGraph.AnyTensorGroup where Element: DynamicGraph.AnyTensor {
   public typealias AnyTensor = [DynamicGraph.AnyTensor]
   public static func exec(cmd: ccv_nnc_cmd_t, hint: ccv_nnc_hint_t, inputs: [AnyTensor], outputSize: Int32, streamContext: StreamContext?) -> [AnyTensor] {
-    return []
+    assert(inputs.count > 0)
+    let graph = inputs[0][0].graph
+    let parallel = inputs[0].count
+    let inputSize = inputs.count
+    var _inputs = [ccv_nnc_tensor_variable_t?](repeating: nil, count: parallel * inputSize)
+    let _outputs = UnsafeMutablePointer<ccv_nnc_tensor_variable_t?>.allocate(capacity: Int(outputSize) * parallel)
+    let outputs: [[DynamicGraph.AnyTensor]] = (0..<outputSize).map { _ in (0..<parallel).map { _ in graph.variable() } }
+    for (i, input) in inputs.enumerated() {
+      assert(input.count == parallel)
+      for (j, tensor) in input.enumerated() {
+        assert(tensor.graph === graph)
+        _inputs[j * inputSize + i] = tensor._tensor
+      }
+    }
+    for (i, output) in outputs.enumerated() {
+      for (j, tensor) in output.enumerated() {
+        (_outputs + j * Int(outputSize) + i).initialize(to: tensor._tensor)
+      }
+    }
+    let _graph = graph._graph
+    let _streamContext = streamContext?._stream
+    ccv_nnc_dynamic_graph_exec(_graph, cmd, hint, 0, _inputs, Int32(inputSize * parallel), _outputs, outputSize * Int32(parallel), Int32(parallel), _streamContext)
+    _outputs.deallocate()
+    return outputs
   }
   public static func evaluate(model: Model, inputs: [AnyTensor], streamContext: StreamContext?) -> [AnyTensor] {
+    assert(inputs.count > 0)
     return []
   }
 }
@@ -88,11 +117,12 @@ extension DynamicGraph.Tensor: DynamicGraph.TensorGroup {
 extension Array: DynamicGraph.TensorGroup where Element: _DynamicGraph_TensorGroup, Element: DynamicGraph.AnyTensor {
   public typealias Element = Element._Element
   public init(_ obj: AnyTensor) {
-    self.init(obj.map { Element($0) })
+    self = obj.map { Element($0) }
   }
 }
 
 public enum Functional {
+  @inlinable
   internal static func exec<T: DynamicGraph.AnyTensorGroup>(_: T.Type, cmd: ccv_nnc_cmd_t, hint: ccv_nnc_hint_t, inputs: [T.AnyTensor], outputSize: Int32, streamContext: StreamContext? = nil) -> [T.AnyTensor] {
     return T.exec(cmd: cmd, hint: hint, inputs: inputs, outputSize: outputSize, streamContext: streamContext)
   }
@@ -102,6 +132,7 @@ public enum Functional {
 }
 
 public extension Model {
+  @inlinable
   internal func callAsFunction<T: DynamicGraph.AnyTensorGroup>(_: T.Type, _ inputs: [T.AnyTensor], streamContext: StreamContext? = nil) -> [T.AnyTensor] {
     return T.evaluate(model: self, inputs: inputs, streamContext: streamContext)
   }
