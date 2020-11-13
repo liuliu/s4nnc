dd168480baf3b91175927135fc49bd86319d8763
Liu Liu <i@liuliu.me>
2020-11-12 20:02:37 -0500

    Implement rand for Group.

--- nnc/FunctionalAddons.swift
+++ nnc/FunctionalAddons.swift
@@ -111,6 +111,28 @@ public extension DynamicGraph.Tensor {
   }
 }
 
+public extension DynamicGraph.Group {
+  func rand(_ lowerBound: Float = 0, _ upperBound: Float = 1, streamContext: StreamContext? = nil) {
+    guard underlying.count > 0 else { return }
+    var params = CmdParamsFactory.factory.newParams()
+    params.size.dim = (1, 1, 1, 0, 0, 0, 0, 0)
+    params.blas.a = (lowerBound, upperBound, 0)
+    let cmd = ccv_nnc_cmd(CCV_NNC_RANDOM_UNIFORM_FORWARD, nil, params, 0)
+    let _graph = underlying[0].graph._graph
+    let _streamContext = streamContext?._stream
+    var _output: ccv_nnc_tensor_variable_t? = underlying[0]._tensor
+    ccv_nnc_dynamic_graph_exec(_graph, cmd, ccv_nnc_no_hint, 0, nil, 0, &_output, 1, 0, _streamContext)
+    ccv_nnc_dynamic_graph_set_no_grad(_graph, 1)
+    let copy = ccv_nnc_cmd(CCV_NNC_DATA_TRANSFER_FORWARD, nil, params, 0)
+    // Init the rest of them to be the same.
+    for rest in underlying.suffix(from: 1) {
+      var _target: ccv_nnc_tensor_variable_t? = rest._tensor
+      ccv_nnc_dynamic_graph_exec(_graph, copy, ccv_nnc_no_hint, 0, &_output, 1, &_target, 1, 0, _streamContext)
+    }
+    ccv_nnc_dynamic_graph_set_no_grad(_graph, 0)
+  }
+}
+
 public extension DynamicGraph.Tensor {
   func toGPU(_ ordinal: Int = 0, streamContext: StreamContext? = nil) -> DynamicGraph.Tensor<Element> {
     var params = CmdParamsFactory.factory.newParams()
