d888319782c53c8016e890236de793706dee9d90
Liu Liu <i@liuliu.me>
2023-03-14 18:35:56 -0400

    Update to avoid direct copy to cpu.

--- nnc/Store.swift
+++ nnc/Store.swift
@@ -141,9 +141,12 @@ extension DynamicGraph {
             TensorFormat.from(cTensorParams: params), TensorShape(dims: params.dim))
           switch result {
           case .final(let tensor):
-            let cTensor = tensor.cTensor
-            let dataSize = ccv_nnc_tensor_data_size(cTensor.pointee.info)
-            ccv_nnc_tensor_swap(cTensorOut, name, dir, cTensor.pointee.data.ptr, dataSize)
+            let cpuTensor = tensor.toCPU()
+            cpuTensor.withUnsafeBytes { _ in
+              let cTensor = cpuTensor.cTensor
+              let dataSize = ccv_nnc_tensor_data_size(cTensor.pointee.info)
+              ccv_nnc_tensor_swap(cTensorOut, name, dir, cTensor.pointee.data.ptr, dataSize)
+            }
             return Int32(CCV_IO_FINAL)
           case .continue(let name):
             return ccv_nnc_tensor_read(readerHelper.sqlite, name, dir, tensorOut)
