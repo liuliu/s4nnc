632b6f2a61003e5557e30aba7943d94772dee92e
Liu Liu <i@liuliu.me>
2023-09-09 16:01:07 -0400

    Only Jit on parameters that is friendly to decode.

--- nnc/Store.swift
+++ nnc/Store.swift
@@ -1981,7 +1981,7 @@ private let q4pDecodeJit:
     for i in 1..<Int(dimensionCount) {
       numberOfElements *= Int(dimensions[i])
     }
-    guard TensorShape(dims: params.dim).reduce(1, *) == numberOfElements else {
+    guard TensorShape(dims: params.dim).reduce(1, *) == numberOfElements && (numberOfElements % blockSize) == 0 else {
       return q4pDecode(
         data, dataSize, dataType, dimensions, dimensionCount, identifier, context, params,
         tensorOut, decoded, decodedSize)
@@ -2030,7 +2030,7 @@ private let q5pDecodeJit:
     for i in 1..<Int(dimensionCount) {
       numberOfElements *= Int(dimensions[i])
     }
-    guard TensorShape(dims: params.dim).reduce(1, *) == numberOfElements else {
+    guard TensorShape(dims: params.dim).reduce(1, *) == numberOfElements && (numberOfElements % blockSize) == 0 else {
       return q5pDecode(
         data, dataSize, dataType, dimensions, dimensionCount, identifier, context, params,
         tensorOut, decoded, decodedSize)
@@ -2079,7 +2079,7 @@ private let q6pDecodeJit:
     for i in 1..<Int(dimensionCount) {
       numberOfElements *= Int(dimensions[i])
     }
-    guard TensorShape(dims: params.dim).reduce(1, *) == numberOfElements else {
+    guard TensorShape(dims: params.dim).reduce(1, *) == numberOfElements && (numberOfElements % blockSize) == 0 else {
       return q6pDecode(
         data, dataSize, dataType, dimensions, dimensionCount, identifier, context, params,
         tensorOut, decoded, decodedSize)
@@ -2128,7 +2128,7 @@ private let q7pDecodeJit:
     for i in 1..<Int(dimensionCount) {
       numberOfElements *= Int(dimensions[i])
     }
-    guard TensorShape(dims: params.dim).reduce(1, *) == numberOfElements else {
+    guard TensorShape(dims: params.dim).reduce(1, *) == numberOfElements && (numberOfElements % blockSize) == 0 else {
       return q7pDecode(
         data, dataSize, dataType, dimensions, dimensionCount, identifier, context, params,
         tensorOut, decoded, decodedSize)
@@ -2177,7 +2177,7 @@ private let q8pDecodeJit:
     for i in 1..<Int(dimensionCount) {
       numberOfElements *= Int(dimensions[i])
     }
-    guard TensorShape(dims: params.dim).reduce(1, *) == numberOfElements else {
+    guard TensorShape(dims: params.dim).reduce(1, *) == numberOfElements && (numberOfElements % blockSize) == 0 else {
       return q8pDecode(
         data, dataSize, dataType, dimensions, dimensionCount, identifier, context, params,
         tensorOut, decoded, decodedSize)
